52319e3dc29d9c980a50ab867a04bb878f38b6c2
loretta chan <lorettachanlt@berkeley.edu>
2017-02-12 11:31:03 -0800

    added boxes

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    var count = 1;
+
+    $("#generate-button").click(function () {
+      var factDiv = document.getElementById("fact");
+      var facts = [
+      "I struggle to keep my eyes opened when being photographed.",
+      "I was born in Hong Kong and raised in a small town outside London called High Wycombe.",
+      "I speak cantonese, mandarin and un pocito de espanol.",
+      "I like to sleep diagonally.",
+      "I can type more than 100 words per minute."
+      ]
+      factDiv.innerHTML = facts[count];
+      if (count >= facts.length - 1) {
+        count = 0;
+      } else {
+        count ++; 
+      }
+    });
+
+});
